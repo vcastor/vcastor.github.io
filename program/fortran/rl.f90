3b2b032b17cf4925a1438079cc40c34c8e195097
vcastor <vcastor@users.noreply.github.com>
2021-08-29 13:57:49 +0200

    better fortran codes

--- program/fortran/rl.f90
+++ program/fortran/rl.f90
@@ -1,28 +1,30 @@
+PROGRAM RegresionLineal
 
-
-parameter (n=100)
-dimension x(n),y(n)
+!==========================
+!Programita para hacer una regesion lineal y el calculo
+!de las incertidumbres
+!VCastor
+!Programa hecho en 2016 cuando recien iniciaba en la programacion
+!esta siendo actualizado en 2021
+!==========================
 
 Integer npuntos
 real m,b,sx,sx2,sy,sy2,sxy,x,y,r,symxb2,ub,um
 
-do i=1,n
-x(i)=0.0
-y(i)=0.0
-end do
+WRITE(*,*) "The data should be in a file with the name:"
+WRITE(*,*) "data.dat"
 
+OPEN(8,FILE="data.dat")
+READ(8,*) npuntos
 
-open(8,file="data.f90")
-read(8,*)npuntos
-write(*,*)npuntos
+dimension x(npuntos),y(npuntos)
+
 do i=1,npuntos
-read(8,*)x(i),y(i)
-write(*,*)x(i),y(i)
+ read(8,*)x(i),y(i)
 end do
-write(*,*)
 
 !==========================
-!PROCEDIMIENTO 
+!PROCEDIMIENTO, sumas
 !==========================
 sx=0.0
 sy=0.0
@@ -30,14 +32,12 @@ sx2=0.0
 sy2=0.0
 sxy=0.0
 do i=1,npuntos
-sx=sx+x(i)
-sy=sy+y(i)
-sx2=sx2+x(i)*x(i)
-sy2=sy2+y(i)*y(i)
-sxy=sxy+x(i)*y(i)
+ sx=sx+x(i)
+ sy=sy+y(i)
+ sx2=sx2+x(i)*x(i)
+ sy2=sy2+y(i)*y(i)
+ sxy=sxy+x(i)*y(i)
 end do
-
-!write(*,*)sx,sx2,sy,sxy
 
 !===========
 !PENDIENTE
@@ -61,14 +61,13 @@ r=(sxy-(sx*sy/npuntos))/(SQRT((sx2-(sx*sx/npuntos))*(sy2-(sy*sy/npuntos))))
 write(*,*)"r=",r
 write(*,*)"r^2=",r*r
 
-
 !========================
 !INCERTIDUMBRES
 !========================
 
 symxb2=0.0
 do i=1,npuntos
-symxb2=symxb2+(y(i)-m*x(i)-b)*(y(i)-m*x(i)-b)
+ symxb2=symxb2+(y(i)-m*x(i)-b)*(y(i)-m*x(i)-b)
 end do
 
 um=SQRT((npuntos*symxb2)/((npuntos-2)*(npuntos*sx2-sx*sx)))
@@ -77,11 +76,4 @@ ub=SQRT((sx2*symxb2)/((npuntos-2)*(npuntos*sx2-sx*sx)))
 write(*,*)"U(m)=",um
 write(*,*)"U(b)=",ub
 
-open(9,file="resultado.csv")
-do i=1, npuntos
-write(9,*)x(i),',', y(i)
-end do
-
-
-stop
-end
+ENDPROGRAM RegresionLineal
